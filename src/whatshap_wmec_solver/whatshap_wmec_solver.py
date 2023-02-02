39811b08e998e54c8c5efcb205b99dc2bbd9cde6
Jia-Yuan Zhang <sloppybaron@gmail.com>
2023-02-02 11:53:53 +0000

    Add type checks

--- src/whatshap_wmec_solver/whatshap_wmec_solver.py
+++ src/whatshap_wmec_solver/whatshap_wmec_solver.py
@@ -2,18 +2,28 @@
 # coding: utf-8
 
 from __future__ import annotations
-from typing import Iterable, Sequence
+from typing import Sequence
+from dataclasses import dataclass
 import numpy as np
 import whatshap.core as wh
 from whatshap.testhelpers import canonic_index_to_biallelic_gt
 
 
+# Type aliases
+Matrix2D = Sequence[Sequence[int]]
+
+@dataclass
+class wMECSolverResult:
+    haplotypes: tuple[Sequence[int], Sequence[int]]
+    partition: Sequence[int]
+    cost: float
+
 class wMECSolver:
     def __init__(self, readset):
-        self.readset = readset
+        self._readset = readset
 
     @classmethod
-    def from_matrix(cls, matrix, weights=None):
+    def from_matrix(cls, matrix: Matrix2D, weights: Matrix2D | None = None):
         rs = wh.ReadSet()
         mapping_quality = 50
         source_id = 0
@@ -31,20 +41,17 @@ class wMECSolver:
             rs.add(read)
         return cls(rs)
 
-    @classmethod
-    def from_matrix_file(cls, matrix_file_handle):
-        matrix = []
-        for line in matrix_file_handle:
-            row = [int(c) if c.isdigit() else -1 for c in line]
-            matrix.append(row)
-        return cls.from_matrix(matrix)
+    # @classmethod
+    # def from_matrix_file(cls, matrix_file_handle):
+    #     matrix = []
+    #     for line in matrix_file_handle:
+    #         row = [int(c) if c.isdigit() else -1 for c in line]
+    #         matrix.append(row)
+    #     return cls.from_matrix(matrix)
 
-    @classmethod
-    def from_compressed_matrix_file(cls, matrix_file):
-        pass
 
-    def compute_haplotypes(self, all_heterozygous=False):
-        positions = self.readset.get_positions()
+    def compute_haplotypes(self, all_heterozygous=False) -> wMECSolverResult:
+        positions = self._readset.get_positions()
         recombcost = [1] * len(positions)
         pedigree = wh.Pedigree(wh.NumericSampleIds())
         genotype_likelihoods = [
@@ -57,7 +64,7 @@ class wMECSolver:
         )
 
         dp_table = wh.PedigreeDPTable(
-            self.readset, recombcost, pedigree, distrust_genotypes=not all_heterozygous
+            self._readset, recombcost, pedigree, distrust_genotypes=not all_heterozygous
         )
 
         superreads, transmission_vector = dp_table.get_super_reads()
@@ -65,22 +72,25 @@ class wMECSolver:
 
         cost = dp_table.get_optimal_cost()
         partition = dp_table.get_optimal_partitioning()
-        hap1 = [v.allele for v in superreads[0][0]]
-        hap2 = [v.allele for v in superreads[0][1]]
+        hap1:list[int] = [v.allele for v in superreads[0][0]]
+        hap2:list[int]  = [v.allele for v in superreads[0][1]]
 
         # Represent ambiguous values with -1 instead of 3
         hap1 = [x if x != 3 else -1 for x in hap1]
         hap2 = [x if x != 3 else -1 for x in hap2]
-        
-        return (hap1, hap2), partition, cost, superreads
+
+        return wMECSolverResult(haplotypes=(hap1, hap2), partition=partition, cost=cost)
+
+
 
 
 def solve_wMEC(
-    allele_matrix: Iterable[Iterable[int]],
-    weights: Iterable[Iterable[int]] | None = None,
+    allele_matrix: Matrix2D,
+    weights: Matrix2D | None = None,
 ) -> tuple[Sequence[int], Sequence[int]]:
     solver = wMECSolver.from_matrix(allele_matrix, weights=weights)
-    (haplotype_1, haplotype_2), *_ = solver.compute_haplotypes()
+    result = solver.compute_haplotypes()
+    haplotype_1, haplotype_2 = result.haplotypes
     if haplotype_1 > haplotype_2:
         haplotype_1, haplotype_2 = haplotype_2, haplotype_1
     return haplotype_1, haplotype_2
